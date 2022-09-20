162de168ec554b9ecfc319f66a9d7307b9ce68dc
iamsangbeom <psb5657@korea.ac.kr>
2022-09-20 13:22:45 +0900

    update kinematics for a tcp joint

--- code/json_loader.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt 
-import json 
-name = '../code/data/grp/manipulator_traj.json'
-json_content = []
-for line in open(name, 'r'):
-    json_content.append(json.loads(line))
-    print("[Number of trajectory: {}] [Number of step: {}] [Joint value: {}]".format(json.loads(line)["num_traj"], json.loads(line)["num_step"], json.loads(line)["q"]))
